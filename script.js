b5fd5b005725a006742c754eddec10dfd37459b3
rahulsingh239 <earphone70022gmail.com>
2026-02-04 09:48:10 +0545

    uhviu

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log("unwanted buggy code")
+console.log("Hello world!")
+
+/* 
+comments
+ctrl + / line comment
+shift + alt +a  block comment
+*/
+
+let color = "pink"
+console.log("Tshering favourite color is ", color)
+let fullname = "Shyam"
+console.log(fullname)
+fullname = "Hari"
+console.log(fullname)
+fullname = "Ram"
+console.log(fullname)
