9705c500fa976cfd772c7b5f4ac649497e2de1c1
rahulsingh239 <earphone70022gmail.com>
2026-02-04 10:30:23 +0545

    My first javascript

--- script.js
+++ script.js
@@ -6,12 +6,59 @@ comments
 ctrl + / line comment
 shift + alt +a  block comment
 */
-
+/* 
 let color = "pink"
-console.log("Tshering favourite color is ", color)
-let fullname = "Shyam"
+console.log("Tshering favourite color is ", color) */
+
+/* let fullname = "Shyam"
 console.log(fullname)
 fullname = "Hari"
 console.log(fullname)
 fullname = "Ram"
-console.log(fullname)
+console.log(fullname) */
+
+/* let firstName = "Hari"
+let middleName = "Kumar"
+middleName = "Bahadur"
+
+let fullName = firstName + " " + middleName
+console.log(fullName) */
+/* 
+const PI = 3.141592653689793;
+// const PI = 3.14;
+
+console.log(PI.toFixed(2)); */
+
+/* let a = 1;
+let b = 2;
+let c;
+console.log(a, b);
+
+c = a;
+a = b;
+b = c;
+console.log(a, b);
+
+console.log("double quote = ",'" "') */
+
+let color ={
+    name: "red",
+    hexValue: "#FF0000",
+    rgbValue: "rgba(255,0,0)"
+}
+console.log(`hex value of color ${color.name} is ${color.hexValue} and rgb value is ${color.rgbValue}.`)
+
+let myLaptop ={
+    name: "DELL",
+    storage: "Ram 16gb"
+}
+console.log(myLaptop)
+
+let person ={
+    first_name: "John",
+    last_name: "doe",
+    age: "15",
+    website: "john@doe.com"
+}
+
+console.log(`My full name is ${person.first_name} ${person.last_name}.`)
